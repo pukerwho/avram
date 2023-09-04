8f0247454645bb38f2026e5c13c8044dfdec125e
Puker Who <nawwesele@gmail.com>
2023-09-04 15:09:44 +0300

    edit

--- single.php
+++ single.php
@@ -52,7 +52,7 @@
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                 </svg>
               </div>
-              <div class="text-gray-700 opacity-75"><?php echo get_the_modified_time('F j, Y') ?></div>
+              <div class="text-gray-700 opacity-75"><?php echo get_the_date('F j, Y') ?></div>
             </div>
             <div class="hidden lg:block px-4">•</div>
             <!-- CATEGORY -->
